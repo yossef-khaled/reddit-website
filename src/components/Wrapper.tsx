fc6d276ea0fcc1d83e93488385eba47ef3b41496
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-17 11:07:10 +0200

    Add a wrapper component

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,19 @@
+//Import from react
+import React, { ReactNode } from "react";
+
+//Import from @chakra-ui/react 
+import { Box } from "@chakra-ui/react";
+
+interface WrapperProps {
+    children: ReactNode;
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+    return (
+        <Box maxW={"600px"} w="70%" mx={'auto'} mt={20}>
+            {children}
+        </Box>
+    )
+}
+
+export default Wrapper;
