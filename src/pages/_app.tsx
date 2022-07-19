337ff9ad560f1c631cddd72ff7b2af977a29226d
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-19 11:15:52 +0200

    Provide urql client to connect w/backend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,29 @@
+//Import from next
+import { AppProps } from 'next/app'
+
+//Import from @chakra-ui
 import { ChakraProvider } from '@chakra-ui/react'
 
+//Import theme
 import theme from '../theme'
-import { AppProps } from 'next/app'
+
+//Import from urql
+import { Provider, createClient } from 'urql';
+
+const client = createClient({ 
+  url : 'http://localhost:4000/graphql',
+  fetchOptions: {
+    credentials: 'include', // If this property is sett to 'include' & the back-end does not provide cors credintials, it will throw a cors error  
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   )
 }
 
