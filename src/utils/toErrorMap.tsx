0508b22ada72f39f770ea4ee1472559d6c4e6d59
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-23 01:48:45 +0200

    Error map function

---
+++ src/utils/toErrorMap.tsx
@@ -0,0 +1,13 @@
+import { FieldError } from "../generated/graphql";
+
+export const toErrorMap = (errors: FieldError[]) => {
+    
+    const errorMap: Record<string, string> = {};
+
+    errors.map(({ field, message }) => {
+        errorMap[field] = message;
+    })
+
+    return errorMap;
+
+}
