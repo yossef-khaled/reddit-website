cd17379e11ab0d6567efbbc963fdfab53a4f7915
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-27 15:30:59 +0200

    Check if user logged in with graphql me query

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,29 @@
 //Import from @chakra-ui/react 
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 
 //Import from next
 import NextLink from 'next/link'; 
+import { useEffect } from "react";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {
     
 }
 
 const NavBar: React.FC<NavBarProps> = ({ }) => {
-    return (
-        <Flex bg={'tomato'} p={'5'}>
-            <Box ml={'auto'}>
+
+    const [ {data, fetching, error}, whoAmI] = useMeQuery();
+
+    var navbarBody = null;
+
+    // Data is loading
+    if(fetching) {
+        
+    // User is not logged in
+    } else if (!data?.me) {
+
+        navbarBody = (
+            <>
                 <NextLink href={'/login'}>
                     <Link m={2}>
                         Login
@@ -22,6 +34,26 @@ const NavBar: React.FC<NavBarProps> = ({ }) => {
                         Register
                     </Link>
                 </NextLink>
+            </>
+        )
+    } else if(data.me) {
+        
+        navbarBody = (
+            <Flex>
+                <Box mr={'2'}>
+                    {data.me.username}
+                </Box>
+                <Button variant='link'>
+                    Logout
+                </Button>
+            </Flex>
+        )
+    }
+
+    return (
+        <Flex bg={'tomato'} p={'5'}>
+            <Box ml={'auto'}>
+                {navbarBody}
             </Box>
         </Flex>
     )
