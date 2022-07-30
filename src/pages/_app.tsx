fbedf0323ed435c2b52dcf3dccd6e8b1d8ea4528
yossef-khaled <yossef.k.y333@gmail.com>
2022-07-30 17:22:46 +0200

    Use the new urql client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,89 +7,11 @@ import { ChakraProvider } from '@chakra-ui/react'
 //Import theme
 import theme from '../theme'
 
-//Import from urql
-import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
-import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
-
-//Import urql auto generated types
-import { MeDocument, LoginMutation, RegisterMutation, MeQuery, LogoutMutation } from '../generated/graphql';
-
-// This function is a better way to declare cache.updateQuery args types
-function betterUpdateQuery<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, (data) => fn(result, data as any) as any)
-}
-
-const client = createClient({ 
-  url : 'http://localhost:4000/graphql',
-  fetchOptions: {
-    credentials: 'include', // If this property is sett to 'include' & the back-end does not provide cors credintials, it will throw a cors error  
-  },
-  // graphql cache stuff
-  exchanges: [dedupExchange, cacheExchange({
-    updates: {
-      Mutation: {
-        login: (_result, args, cache, info) => {
-          betterUpdateQuery<LoginMutation, MeQuery> (
-            cache,
-            {query: MeDocument},
-            _result,
-            (result, query) => {
-              if(result.login.errors) {
-                return query
-              } 
-              return {
-                me: result.login.user,
-              }
-            }
-          )
-        },
-        register: (_result, args, cache, info) => {
-          betterUpdateQuery<RegisterMutation, MeQuery> (
-            cache,
-            {query: MeDocument},
-            _result,
-            (result, query) => {
-              if(result.register.errors) {
-                return query
-              } 
-              return {
-                me: result.register.user,
-              }
-            }
-          )
-        },
-        logout: (_result, args, cache, info) => {
-          betterUpdateQuery<LogoutMutation, MeQuery> (
-            cache,
-            {query: MeDocument},
-            _result,
-            (result, query) => {
-              if(!result.logout) {
-                return query;
-              } 
-              return {
-                me: null,
-              }
-            }
-          )
-        },
-      },
-    }
-  }), fetchExchange],
-})
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider value={client}>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
-    </Provider>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,58 @@
+import React from "react";
+
 //Import other components
 import NavBar from "../components/NavBar"
 
-const Index = () => (
-  <>
-    <NavBar/>
-    <div>
-      Hello world
-    </div>
-  </>
-)
+//Import my urql client
+import createUrqlClient from "../utils/createUrqlClient";
 
-export default Index
+//Import from next-urql
+import { withUrqlClient } from 'next-urql'; 
+import { usePostsQuery } from "../generated/graphql";
+import { useEffect, useState } from "react";
+
+interface IState {
+  waitingDots: string,
+}
+
+const Index = () => {
+
+  const [{data}] = usePostsQuery();
+  const [waitingDots, setWaitingDots] = useState('');
+
+  const timerForLoadingDots = setTimeout(() => {
+      waitingDots.length == 3 ? setWaitingDots('')
+      :
+      setWaitingDots(`${waitingDots}.`);
+    }
+  , 500) 
+
+  useEffect(() => {
+    timerForLoadingDots;
+  }, []);
+
+  if(data?.posts) {
+    clearTimeout(timerForLoadingDots);
+  }
+  
+  return (
+    <>
+      <NavBar/>
+      <div>
+        {!data?.posts ? 
+          <div>Loading{waitingDots}</div>
+        :
+        
+        data.posts.map((post) => {
+          return (
+            <p key={post.id}>
+              {post.title}
+            </p>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default  withUrqlClient(createUrqlClient)(Index);
