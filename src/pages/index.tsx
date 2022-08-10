4e28a9ddcb48b5fa42288c98c68e6c64e0d28bc1
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-10 13:37:15 +0200

    Display posts in a better way

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
+//Import from react
 import React, { FC } from "react";
-
-//Import other components
-import NavBar from "../components/NavBar"
 
 //Import my urql client
 import createUrqlClient from "../utils/createUrqlClient";
+
+//Import from @chakra
+import { Box, Button, Heading, Stack, Text } from "@chakra-ui/react";
 
 //Import from next-urql
 import { withUrqlClient } from 'next-urql'; 
 import { usePostsQuery } from "../generated/graphql";
 import { useEffect, useState } from "react";
+
+//Import other components
+import Layout from "../components/layout";
+
+//Import from next
+import NextLink from 'next/link'; 
 
 interface IState {
   waitingDots: string,
@@ -17,7 +24,11 @@ interface IState {
 
 const Index: FC<IState> = () => {
 
-  const [{data}] = usePostsQuery();
+  const [{data}] = usePostsQuery({
+    variables: {
+      limit: 10
+    }
+  });
   const [waitingDots, setWaitingDots] = useState('');
 
   const timerForLoadingDots = setTimeout(() => {
@@ -36,21 +47,29 @@ const Index: FC<IState> = () => {
   }
   
   return (
-    <>
-      <NavBar/>
+    <Layout>
+      <NextLink href={'/create-post'}>
+        <Button backgroundColor={'wheat'} mb={5}>
+          Create Post
+        </Button>
+      </NextLink>
       <div>
         {!data?.posts ? 
           <div>Loading{waitingDots}</div>
         :
-        data.posts.map((post) => {
-          return (
-            <p key={post.id}>
-              {post.title}
-            </p>
-          )
-        })}
+        <Stack spacing={8} marginBottom={8}>
+          {data.posts.map((post) => {
+            return (
+              <Box p={5} key={post.id} shadow='md' borderWidth='1px'>
+                <Heading fontSize='xl'>{post.title}</Heading>
+                <Text mt={4}>{post.text}</Text>
+              </Box>
+            )
+          })}
+        </Stack>  
+        }
       </div>
-    </>
+    </Layout>
   )
 }
 
