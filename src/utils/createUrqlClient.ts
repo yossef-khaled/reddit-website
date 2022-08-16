9508e1f3eacf38df582cf4b56a2d637573a41ff0
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-16 13:17:37 +0200

    Add pagination with ineraction with the cache

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,10 +1,10 @@
 //Import from urql 
-import { dedupExchange, fetchExchange, Exchange } from "urql";
+import { dedupExchange, fetchExchange, Exchange, stringifyVariables } from "urql";
 
 //Import from exchange-graphcache
-import { cacheExchange } from "@urql/exchange-graphcache";
+import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
 
-//Improt auto generated types
+//Import auto generated types
 import { LoginMutation, MeQuery, MeDocument, RegisterMutation, LogoutMutation } from "../generated/graphql";
 
 //Import others 
@@ -15,6 +15,96 @@ import { filter, pipe, tap } from 'wonka';
 
 //Import from next router
 import Router from "next/router";
+
+export const cursorPagination = (): Resolver => {
+  
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+    
+    const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+    const isItInTheCache = cache.resolve(
+      cache.resolve(entityKey, fieldKey) as string, 
+      'posts'
+    );
+    info.partial = !isItInTheCache;
+    const allFields = cache.inspectFields(entityKey);
+    const fieldInfos = allFields.filter(info => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+
+    let results: string[] = [];
+    let hasMore = true;
+    fieldInfos.forEach((fi) => {
+      const key = cache.resolve(entityKey, fi.fieldKey) as string;
+      const data = cache.resolve(key, "posts") as string[];
+      const _hasMore = cache.resolve(key, "hasMore");
+      if(!_hasMore) {
+        hasMore = _hasMore as boolean;
+      }
+      results.push(...data);
+    })
+
+    return {
+      __typename: 'PaginatedPosts',
+      hasMore: hasMore,
+      posts: results
+    };
+
+    // const visited = new Set();
+    // let result: NullArray<string> = [];
+    // let prevOffset: number | null = null;
+
+    // for (let i = 0; i < size; i++) {
+    //   const { fieldKey, arguments: args } = fieldInfos[i];
+    //   if (args === null || !compareArgs(fieldArgs, args)) {
+    //     continue;
+    //   }
+
+    //   const links = cache.resolve(entityKey, fieldKey) as string[];
+    //   const currentOffset = args[cursorArgument];
+
+    //   if (
+    //     links === null ||
+    //     links.length === 0 ||
+    //     typeof currentOffset !== 'number'
+    //   ) {
+    //     continue;
+    //   }
+
+    //   const tempResult: NullArray<string> = [];
+
+    //   for (let j = 0; j < links.length; j++) {
+    //     const link = links[j];
+    //     if (visited.has(link)) continue;
+    //     tempResult.push(link);
+    //     visited.add(link);
+    //   }
+
+    //   if (
+    //     (!prevOffset || currentOffset > prevOffset) ===
+    //     (mergeMode === 'after')
+    //   ) {
+    //     result = [...result, ...tempResult];
+    //   } else {
+    //     result = [...tempResult, ...result];
+    //   }
+
+    //   prevOffset = currentOffset;
+    // }
+
+    // const hasCurrentPage = cache.resolve(entityKey, fieldName, fieldArgs);
+    // if (hasCurrentPage) {
+    //   return result;
+    // } else if (!(info as any).store.schema) {
+    //   return undefined;
+    // } else {
+    //   info.partial = true;
+    //   return result;
+    // }
+  };
+};
 
 export const errorExchange: Exchange = ({ forward }) => ops$ => {
   return pipe(
@@ -34,6 +124,14 @@ const createUrqlClient = (ssrExchange: any) => ({
     },
     exchanges: [dedupExchange, 
         cacheExchange({
+            keys: {
+              PaginatedPosts: () => null
+            },
+            resolvers: {
+              Query: {
+                posts: cursorPagination(), // key should match the mutation in the graphql file        
+              }
+            },
             updates: {
                 Mutation: {
                     login: (_result, args, cache, info) => {
