bc523c604bf8a257220278c0af791e72c95bb14e
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-29 15:01:28 +0200

    Add UI for post delete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,13 @@ import React, { FC } from "react";
 import createUrqlClient from "../utils/createUrqlClient";
 
 //Import from @chakra
-import { Box, Button, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, IconButton, Link, Stack, Text } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 //Import from next-urql
 import { withUrqlClient } from 'next-urql'; 
-import { usePostsQuery } from "../generated/graphql";
-import { useEffect, useState } from "react";
+import { useDeletePostMutation, usePostsQuery } from "../generated/graphql";
+import { useState } from "react";
 
 //Import other components
 import Layout from "../components/layout";
@@ -18,6 +19,7 @@ import Layout from "../components/layout";
 //Import from next
 import NextLink from 'next/link'; 
 import { UpdootSection } from "../components/UpdootSection";
+import LoadingDots from "../components/LoadingDots";
 
 interface IState {
   waitingDots: string,
@@ -26,29 +28,14 @@ interface IState {
 const Index: FC<IState> = () => {
 
   const [variables, setVariables] = useState({ 
-    limit: 1, 
+    limit: 10, 
     cursor: null as null | string
   })
   const [{data, fetching}] = usePostsQuery({
     variables
   });
-  const [waitingDots, setWaitingDots] = useState('');
 
-  // TODO : use setInterval instead
-  const timerForLoadingDots = setTimeout(() => {
-      waitingDots.length == 3 ? setWaitingDots('')
-      :
-      setWaitingDots(`${waitingDots}.`);
-    }
-  , 500) 
-
-  useEffect(() => {
-    timerForLoadingDots;
-  }, []);
-
-  if(data?.posts.posts) {
-    clearTimeout(timerForLoadingDots);
-  }
+  const [_, deletePost] = useDeletePostMutation();
 
   if(fetching && !data?.posts) {
     return (
@@ -72,19 +59,36 @@ const Index: FC<IState> = () => {
       </Flex>
       <div>
         {fetching && !data?.posts.posts ? 
-          <div>Loading{waitingDots}</div>
+          <LoadingDots isLoading={fetching}/>
         :
         <Stack spacing={8} marginBottom={8}>
           {data?.posts.posts?.map((post) => {
             return (
+              !post ? null
+              :
               <Flex p={5} key={post.id} shadow='md' borderWidth='1px'>
                 <UpdootSection post={post}/>
-                <Box>
-                  <Heading fontSize='2xl'>{post.title}</Heading>
+                <Box flex={1}>
+                    <NextLink href='post/[id]' as={`/post/${post.id}`}>
+                      <Link>
+                        <Heading fontSize='2xl'>{post.title}</Heading>
+                      </Link>
+                    </NextLink>
                   By {' '}
                   <Text fontWeight={'bold'} fontSize={'large'} display={'inline'} mt={4}>{post.creator.username}</Text>
-                  <Text mt={4}>{post.textSnippet}</Text>
-                </Box>
+                  <Flex>
+                    <Text flex={1} mt={4}>{post.textSnippet}</Text>
+                    <IconButton
+                      onClick={() => {
+                        deletePost({
+                          id: post.id
+                        })
+                      }}
+                      aria-label='Delete Post'
+                      icon={<DeleteIcon name="delete"/>}
+                    />
+                  </Flex>
+                  </Box>
               </Flex>
             )
           })}
@@ -96,7 +100,7 @@ const Index: FC<IState> = () => {
             disabled={!data.posts.hasMore}
               onClick={() => {
                   setVariables({
-                    limit: 1,
+                    limit: 10,
                     cursor: data.posts.posts[data.posts.posts.length - 1].createdAt
                   })
               }}  
