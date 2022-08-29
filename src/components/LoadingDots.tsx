d31722827ad9ac55cb8db21122e033ea86fd1eaf
yossef-khaled <yossef.k.y333@gmail.com>
2022-08-29 14:57:31 +0200

    Create a separate component for the loading dots

---
+++ src/components/LoadingDots.tsx
@@ -0,0 +1,34 @@
+import { FC, useEffect, useState } from "react";
+
+interface LoadingDotsProps {
+    isLoading: boolean;
+
+}
+
+const LoadingDots: FC<LoadingDotsProps> = ({isLoading, ...props}) => {
+    const [waitingDots, setWaitingDots] = useState('');
+
+    // TODO : use setInterval instead
+    const timerForLoadingDots = setTimeout(() => {
+        waitingDots.length == 3 ? setWaitingDots('')
+        :
+        setWaitingDots(`${waitingDots}.`);
+      }
+    , 500) 
+  
+    useEffect(() => {
+      timerForLoadingDots;
+    }, []);
+
+    if(!isLoading) {
+        clearTimeout(timerForLoadingDots);
+    }
+    
+    return (
+        <div>
+            Loading{waitingDots}
+        </div>
+    )
+}
+
+export default LoadingDots;
